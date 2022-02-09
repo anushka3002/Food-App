483d3eea54847d8d5c14a8f7a09822774a3a0890
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-02-09 18:09:24 +0530

    Add files via upload

---
+++ S3_d3_import_export/navbar.js
@@ -0,0 +1,21 @@
+function navbar(){
+    return  `<div id="containerff">
+    <div id="navbar">
+        <div id="foodword">
+           <a href="index.html"><p class="pfood">Latest Food</p></a>
+           <a href="food_random.html"><p class="pfood2">Recipe Of The Day</p></a>
+        </div>
+        <div>
+            <img src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png"  width="290px" height="60px" id="logoff">
+        </div>
+        <div id="infoff"><p>Discover the best food & drinks in your place</p></div>
+        <div><input id="inputh" type="text" placeholder="Search for a dish"></div>
+        <div id="foodie"></div>
+        <div id="containerkk">
+        <div id="litfood"></div>
+        <div id="details">
+        </div></div>
+    </div>
+</div>`
+}
+export default navbar
---
+++ S3_d3_import_export/navbar2.js
@@ -0,0 +1,11 @@
+
+function navbar2(){
+    return `<div id="navbar2">
+    <img src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png" width="120px" height="30px" id="home">
+    <div id="blackfood">
+    <a href="index.html"><h3 id="white">Latest food</h3></a>
+    <a href="food_random.html"><h3 id="white">Recipe of the day</h3></a>
+    </div>
+</div>`
+}
+export default navbar2
